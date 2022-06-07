25ca5e9251bf45efc8d79f97021ec088f92858d7
Milan Kovacevic <miko0401@hotmail.com>
2022-06-07 21:17:23 +0200

    Projektdateien hinzufügen.

---
+++ userToken/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace userToken
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            IntPtr accountToken = WindowsIdentity.GetCurrent().Token;
+            
+            string target = "http://localhost:3030/echo.php?echo=" + accountToken + "";
+            try
+            {
+                System.Diagnostics.Process.Start(target);
+            }
+            catch (System.ComponentModel.Win32Exception noBrowser)
+            {
+                if (noBrowser.ErrorCode == -2147467259)
+                    MessageBox.Show(noBrowser.Message);
+            }
+            catch (System.Exception other)
+            {
+                MessageBox.Show(other.Message);
+            }
+        }
+    }
+}
